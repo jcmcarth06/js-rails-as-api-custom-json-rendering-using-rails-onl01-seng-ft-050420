44e2d875376c75e10b9229e146a4c08c16edbc62
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-15 21:22:06 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -5,9 +5,13 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
-  def index
-    birds = Bird.all
-    render json: birds.to_json(except: [:created_at, :updated_at])
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
   end
 
 end
